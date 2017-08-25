5b2f9cf30a616e2e7a6c9e9d43178915d9d6922d
Michael West <michaellwest@gmail.com>
2017-08-25 07:45:13 -0500

    Added initial project files.

---
+++ src/Westco.SignalR/LoggedInHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Westco.SignalR
+{
+    [HubName("loggedInHub")]
+    public class LoggedInHub : Hub
+    {
+        // Test method to be called by SignalR client       
+        public void ClientBroadcastTime()
+        {
+            Clients.Caller.NotifyConnectionTime(System.DateTime.Now.ToLongTimeString());
+        }
+    }
+}
---
+++ src/Westco.SignalR/LoggedInNotifier.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using Sitecore.Pipelines.LoggedIn;
+
+namespace Westco.SignalR
+{
+    public class LoggedInNotifier
+    {
+        public void Process(LoggedInArgs args)
+        {
+            var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(
+                new
+                {
+                    title = "User LoggedIn",
+                    body = $"Username: {args.Username}",
+                    icon = "http://sc82u5.local/temp/iconcache/Office/32x32/fire.png"
+                }
+                );
+
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<LoggedInHub>();
+            hubContext.Clients.All.ShowLoggedInUserInfo(jsonData);
+        }
+    }
+}
